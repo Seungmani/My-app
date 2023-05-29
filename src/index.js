c39e5bb58313ff6033f46695ce14d9ff801835d7
seungmani <dkelcncjs@naver.com>
2023-05-29 18:37:07 +0900

    forms

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ import Accommodate from './chapter_07/accommodate';
 import ConfirmButton from './chapter_08/confirmButton';
 import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
-
+import SignUp from './chapter_11/SingUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AttendanceBook />
+    <SignUp />
   </React.StrictMode>
 );
 
