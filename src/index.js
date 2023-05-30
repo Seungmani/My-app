4dbbe230abbf861ee59d8c6d62dce2f0eec0b4cd
seungmani <dkelcncjs@naver.com>
2023-05-30 11:09:01 +0900

    composition

--- src/index.js
+++ src/index.js
@@ -14,11 +14,12 @@ import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SingUp';
 import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/Profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <ProfileCard />
   </React.StrictMode>
 );
 
